6461c917fe7fda22c4a68fbdc6733bb50b64b645
Brian Pugh <bnp117@gmail.com>
2018-10-09 17:55:23 -0700

    custom fonts

---
+++ custom_fonts/custom_fonts.mk
@@ -0,0 +1,6 @@
+CSRCS += orange_kid.c
+CSRCS += f_6x10.c
+CSRCS += synchronizer7.c
+DEPPATH += --dep-path jolt_gui/custom_fonts
+VPATH += :jolt_gui/custom_fonts
+CFLAGS += "-I$(LVGL_DIR)/jolt_gui/custom_fonts"
